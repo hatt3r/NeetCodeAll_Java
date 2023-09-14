d5da4c317e8adc9574d20835f5b6944d89dc9085
Serene <serene719@gmail.com>
2023-09-14 13:29:46 +0530

    move zeroes, remove duplicates 1 and 2

---
+++ Two Pointers/Remove-Duplicates-from-Sorted-Array-II.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length<3) return nums.length;
+
+        int l=0,r=0;
+        while(l<nums.length)
+        {
+            nums[r++]=nums[l++];
+            if(l<nums.length && nums[l]==nums[r-1])
+            {
+                nums[r++]=nums[l++];
+            }
+            while(l<nums.length && nums[l]==nums[r-1])
+            {l++;}
+        }
+        return r;
+    }
+}
